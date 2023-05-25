154c7b88f5297ccea6e280c23d6425fc20f33e21
Lucas Augusto <126918429+LucasAugustoSS@users.noreply.github.com>
2023-05-25 07:32:15 -0300

    Final boss added.

--- main.js
+++ main.js
@@ -107,16 +107,26 @@ function GameLogic() {
   var doorX;
   var doorY;
   var spike;
-  var button
-  var buttonX
-  var buttonY
-  var button2X
-  var button2Y
+  var spikeX;
+  var spikeY;
+  var newSpike;
+  var spikeArray;
+  var spikeMoving;
+  var spikeInterval;
+  var dialogueTimer;
+  var attackTimer;
+  var button;
+  var buttonX;
+  var buttonY;
+  var button2X;
+  var button2Y;
   var level1;
-  var level2
-  var levelLogic
-  var levelIndex
-  var gameOver
+  var level2;
+  var level3;
+  var level4;
+  var levelLogic;
+  var levelIndex;
+  var gameOver;
 
   level1 = [
     "**************",
@@ -171,7 +181,7 @@ function GameLogic() {
 
   level3 = [
     "***********************************************************",
-    "*&                 ####*          __            __        *",
+    "*                  ####*          __            __        *",
     "*##       #          ##*         |  |          | .|       *",
     "*##### #  ##  #  ######*        <|}[]          []{|>      *",
     "*    # #  #       ###  *         | `|          |  |       *",
@@ -187,7 +197,7 @@ function GameLogic() {
     "*        ##  #####     *                                  *",
     "*  #######       ###   *          __            __        *",
     "*  #     #             *         |  |          | .|       *",
-    "*  #     #         @   *        <|}[]          []{|<      *",
+    "*  #     #             *        <|}[]          []{|<      *",
     "*  # #   ########      *         | `|          |  |       *",
     "*  # ## ##      #  #   *          ‾‾            ‾‾        *",
     "*  #  #         #  #   *                                  *",
@@ -210,7 +220,7 @@ function GameLogic() {
     "*#### ##### # #### ####*        <|}[]          []{|<      *",
     "*      ##      #  ## # *         | `|          |  |       *",
     "* ##        #    # # # *          ‾‾            ‾‾        *",
-    "* *********************************************************",
+    "***********************************************************",
     "*                                                         *",
     "*                    ,----------------,                   *",
     "*                  /‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\                 *",
@@ -225,12 +235,39 @@ function GameLogic() {
     "*                  ‾‾‾‾‾‾‾‾‾‾| |‾‾‾‾‾‾‾‾‾                 *",
     "*                   ___________________                   *",
     "*                  /-.-.-.-.-.-.-.--.-.-\   _|_           *",
-    "*                 /-.-.-..-..-..--..--.-.\  o|o           *",
+    "*                 /-.-.-..-..-..--..--.-.\   |            *",
     "*                 ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾    | |           *",
     "*                             >              ‾            *",
     "*                                                         *",                        
     "***********************************************************",
-  
+  ];
+
+  level4 = [
+    "*****************************************************************************************",
+    "*                                      *          *                                     *",
+    "*                                      *          *                                     *",
+    "*                                      *   \\__/   *                                     *",
+    "*                                      *          *                                     *",
+    "*                                      ************                                     *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*                                                                                       *",
+    "*****************************************************************************************",
+    "                                                                                         ",
   ];
 
 
@@ -251,8 +288,18 @@ function GameLogic() {
   doorY = 3;
 
   spike = "#"
+  spikeX = []
+  spikeY = []
+  spikeArray = []
 
   button = "O"
+
+  teleport = ">"
+  teleportX = null
+  teleportY = null
+  teleport2X = null
+  teleport2Y = null
+
   
   doorOpen = false;
   keyUsed = false;  
@@ -265,12 +312,24 @@ function GameLogic() {
       gameOver = false 
       if (levelIndex === 2){
         levelLogic = level2
+      } else if (levelIndex === 3){
+        levelLogic = level3
+      } else if (levelIndex === 4){
+        levelLogic = level4
       }
-      playerX = 1
-      playerY = 1
-      doorOpen = false
-      keyUsed = false
-      LevelMap()
+      if (levelIndex !== 4) {
+        playerX = 1;
+        playerY = 1;
+      } else {
+        playerX = 45;
+        playerY = 12;
+      }
+      doorOpen = false;
+      keyUsed = false;
+      LevelMap();
+      if (levelIndex === 4) {
+        Ethereo();
+      }
     }
   })
 
@@ -279,17 +338,43 @@ function GameLogic() {
   var gameLevel = "";
   
   if (gameOver === true) {
-    document.getElementById("game-levelone").innerHTML = `<center>Game Over <br><br><br><br> aperte enter para tentar novamente</center>`;
-    keyX = 26;
-    keyY = 3;
-    buttonX = 26;
-    buttonY = 10;
-    button2X = 26;
-    button2Y = 26;
-    keyUsed = false;
-    buttonPressed = false;
-    button2Pressed = false;
-    return;
+    if (levelIndex === 2) {
+        document.getElementById("game-levelone").innerHTML = `<center>Game Over <br><br><br><br> aperte enter para tentar novamente</center>`;
+        keyX = 26;
+        keyY = 3;
+        buttonX = 26;
+        buttonY = 10;
+        button2X = 26;
+        button2Y = 26;
+        keyUsed = false;
+        buttonPressed = false;
+        button2Pressed = false;
+        return;
+    } else if (levelIndex === 3) {
+        document.getElementById("game-levelone").innerHTML = `<center>Game Over <br><br><br><br> aperte enter para tentar novamente</center>`;
+        keyX = 19;
+        keyY = 17;
+        buttonX = 43;
+        buttonY = 55;
+        button2X = 45;
+        button2Y = 55;
+        keyUsed = false;
+        buttonPressed = false;
+        button2Pressed = false;
+        return;
+    } else if (levelIndex === 4) {
+        document.getElementById("game-levelone").innerHTML = `<center>Game Over <br><br><br><br> aperte enter para tentar novamente</center>`;
+        keyX = null;
+        keyY = null;
+        buttonX = 43;
+        buttonY = 2;
+        button2X = 46;
+        button2Y = 2;
+        keyUsed = false;
+        buttonPressed = false;
+        button2Pressed = false;
+        return;
+    }
   }
 
   for (var y = 0; y < levelLogic.length; y++) {
@@ -329,8 +414,75 @@ function GameLogic() {
           console.log(playerLifes);
           return;
         }
-        if (levelIndex === 2 && levelLogic[y][x] === spike && playerLifes === 1) {
-          gameOver = true
+        else if (levelIndex === 3 && levelLogic[y][x] === spike && playerLifes === 3) {
+          playerX = 1;
+          playerY = 1;
+          buttonX = 43;
+          buttonY = 55;
+          button2X = 45;
+          button2Y = 55;
+          doorOpen = false;
+          keyUsed = false;
+          buttonPressed = false;
+          button2Pressed = false;
+          playerLifes = 2;
+          LevelMap()
+          console.log(playerLifes);
+          return;
+        }
+        else if (levelIndex === 3 && levelLogic[y][x] === spike && playerLifes === 2) {
+          playerX = 1;
+          playerY = 1;
+          buttonX = 43;
+          buttonY = 55;
+          button2X = 45;
+          button2Y = 55;
+          doorOpen = false
+          keyUsed = false
+          buttonPressed = false;
+          button2Pressed = false;
+          playerLifes = 1;
+          LevelMap()
+          console.log(playerLifes);
+          return;
+        }
+        else if (levelIndex === 4 && spikeX.indexOf(x) !== -1 && spikeY[spikeX.indexOf(x)] === y && playerLifes === 3) {
+          playerX = 45;
+          playerY = 12;
+          buttonX = 43;
+          buttonY = 2;
+          button2X = 46;
+          button2Y = 2;
+          doorOpen = false;
+          keyUsed = false;
+          buttonPressed = false;
+          button2Pressed = false;
+          playerLifes = 2;
+          LevelMap()
+          console.log(playerLifes);
+          return;
+        }
+        else if (levelIndex === 4 && spikeX.indexOf(x) !== -1 && spikeY[spikeX.indexOf(x)] === y && playerLifes === 2) {
+          playerX = 45;
+          playerY = 12;
+          buttonX = 43;
+          buttonY = 2;
+          button2X = 46;
+          button2Y = 2;
+          doorOpen = false;
+          keyUsed = false;
+          buttonPressed = false;
+          button2Pressed = false;
+          playerLifes = 1;
+          LevelMap()
+          console.log(playerLifes);
+          return;
+        }
+        if (
+          ((levelIndex === 2 || levelIndex === 3) && levelLogic[y][x] === spike && playerLifes === 1) ||
+          (levelIndex === 4 && spikeX.indexOf(x) !== -1 && spikeY[spikeX.indexOf(x)] === y && playerLifes === 1)
+        ) {
+          gameOver = true;
           playerLifes = 3;
           LevelMap();
           return;
@@ -343,6 +495,8 @@ function GameLogic() {
             gameLevelRow += "=";
           } else if (levelIndex === 2 && keyUsed) {
             gameLevelRow += "=";
+          } else if (levelIndex === 3 && keyUsed) {
+            gameLevelRow += "=";
           }
         } else {
           gameLevelRow += door;
@@ -351,8 +505,14 @@ function GameLogic() {
         gameLevelRow += button
       } else if (button2X === x && button2Y === y) {
         gameLevelRow += button
+      } else if (teleportX === x && teleportY === y) {
+        gameLevelRow += teleport
+      } else if (teleport2X === x && teleport2Y === y) {
+        gameLevelRow += teleport
       } else if ((x === 8 && y >= 1 && y <= 5) && (buttonPressed && button2Pressed)) {
         gameLevelRow += " ";
+      } else if (spikeX.indexOf(x) !== -1 && spikeY[spikeX.indexOf(x)] === y) {
+        gameLevelRow += spike
       } else {
         gameLevelRow += levelLogic[y][x];
       }
@@ -387,18 +547,123 @@ function GameLogic() {
     return;
   }
   if (levelIndex === 2 && playerX === doorX && playerY === doorY && doorOpen) {
+    
     levelLogic = level3;
     levelIndex = 3;
+
+    playerX = 1;
+    playerY = 1;
+
+    keyX = 19
+    keyY = 17
+
+    doorX = 23
+    doorY = 35
+
+    buttonX = 43
+    buttonY = 55
+    button2X = 45
+    button2Y = 55
+
+    teleportX = 51
+    teleportY = 3
+    teleport2X = 30
+    teleport2Y = 57
+
+    doorOpen = false;
+    keyUsed = false;
+    buttonPressed = false;
+    button2Pressed = false;
+    LevelMap();
+    return;
   }
 
   document.getElementById("game-levelone").innerHTML = gameLevel;
-}
+  }
 
   function levelWall(x, y) {
     if ((x === 8 && y >= 1 && y <= 5) && (buttonPressed && button2Pressed)) {
       return false;
     }
     return levelLogic[y][x] === "*";
+  }
+
+  function Ethereo() {
+    clearInterval(spikeMoving)
+    clearInterval(spikeInterval)
+    clearTimeout(dialogueTimer)
+    clearTimeout(attackTimer)
+
+    spikeX = []
+    spikeY = []
+    spikeArray = []
+
+    function dialogue() {
+        levelLogic[24] = "diálogo"
+        LevelMap()
+    }
+
+    function moveSpike() {
+      for (var i = 0; i < spikeArray.length; i++) {
+        spikeArray[i].y++;
+
+        spikeX[i] = spikeArray[i].x;
+        spikeY[i] = spikeArray[i].y;
+        
+        if (spikeArray[i].y === 22) {
+            spikeArray[i].x = null
+        }
+
+        LevelMap()
+      }
+    }
+
+    function addSpike() {
+      newSpike = {
+        x: Math.floor(Math.random()*88)+1,
+        y: 1
+      }
+      if (newSpike.x > 38 && newSpike.x < 51) {
+          newSpike.y = 6;
+      }
+      
+      spikeX.push(newSpike.x);
+      spikeY.push(newSpike.y);
+      spikeArray.push(newSpike)
+    }
+    
+    setTimeout(dialogue, 1500)
+    dialogueTimer = setTimeout(function() {
+      spikeMoving = setInterval(moveSpike, 500)
+      spikeInterval = setInterval(addSpike, 750)
+      attackTimer = setTimeout(function() {
+        clearInterval(spikeMoving);
+        clearInterval(spikeInterval);
+        spikeX = []
+        spikeY = []
+        spikeArray = []
+        teleportX = 45;
+        teleportY = 12;
+
+        function dialogue2() {
+          levelLogic[24] = "mais diálogo"
+          LevelMap()
+        }
+
+        function vulnerable() {
+          buttonX = null;
+          buttonY = null;
+          teleport2X = 43;
+          teleport2Y = 2;
+        }
+
+        LevelMap()
+
+        setTimeout(dialogue2, 1500)
+        setTimeout(vulnerable, 3000 /*placeholder*/)
+      }, 15000);
+    }, 3000 /*placeholder*/)
+    return;
   }
 
   document.addEventListener("keydown", (event) => {
@@ -419,34 +684,165 @@ function GameLogic() {
         keyUsed = true;
         keyX = null;
         keyY = null;
-      } else if (buttonX === playerX && buttonY === playerY) {
+      } else if (levelIndex === 2 && buttonX === playerX && buttonY === playerY) {
         buttonPressed = true
         buttonX = null
         buttonY = null
-      } else if (button2X === playerX && button2Y === playerY) {
+      } else if (levelIndex === 2 && button2X === playerX && button2Y === playerY) {
         button2Pressed = true
         button2X = null
         button2Y = null
+      } else if (
+        levelIndex === 3 && (
+          (buttonX === playerX && buttonY === playerY) ||
+          (levelIndex === 3 && button2X === playerX && button2Y === playerY)
+        )
+      ) {
+        levelLogic = level4;
+        levelIndex = 4;
+
+        playerX = 45;
+        playerY = 12;
+
+        keyX = null;
+        keyY = null;
+
+        doorX = null;
+        doorY = null;
+
+        buttonX = 43;
+        buttonY = 2;
+        button2X = 46;
+        button2Y = 2;
+
+        teleportX = null;
+        teleportY = null;
+        teleport2X = null;
+        teleport2Y = null;
+
+        doorOpen = false;
+        keyUsed = false;
+        buttonPressed = false;
+        button2Pressed = false;
+        LevelMap();
+        Ethereo()
+        return;
       }
     //devmode(cheats) abaixo ;)
     } else if (event.key === "v") {
-      buttonX = true;
-      buttonY = true;
-      button2X = true;
-      button2Y = true;
-      buttonPressed = true;
-      button2Pressed = true;
+      if (!(levelIndex === 3)) {
+        buttonX = true;
+        buttonY = true;
+        button2X = true;
+        button2Y = true;
+        buttonPressed = true;
+        button2Pressed = true;
+      }
       keyUsed = true;
       doorOpen = true;
-    }
     //devmode(cheats) acima ;)
 
+    //level selector abaixo
+    } else if (event.key === "1") {
+      levelLogic = level1;
+      levelIndex = 1
+      keyX = 10;
+      keyY = 3;
+      doorX = 0;
+      doorY = 3;
+      buttonX = null;
+      buttonY = null;
+      button2X = null;
+      button2Y = null;
+      teleportX = null;
+      teleportY = null;
+      teleport2X = null;
+      teleport2Y = null;
+      doorOpen = false;
+      keyUsed = false;  
+      buttonPressed = false;
+      button2Pressed = false;
+      LevelMap()
+    } else if (event.key === "2") {
+      levelLogic = level2;
+      levelIndex = 2
+      keyX = 26;
+      keyY = 3;
+      doorX = 0;
+      doorY = 26;
+      buttonX = 26;
+      buttonY = 10;
+      button2X = 26;
+      button2Y = 26;
+      teleportX = null;
+      teleportY = null;
+      teleport2X = null;
+      teleport2Y = null;
+      doorOpen = false;
+      keyUsed = false;  
+      buttonPressed = false;
+      button2Pressed = false;
+      LevelMap()
+    } else if (event.key === "3") {
+      levelLogic = level3;
+      levelIndex = 3
+      keyX = 19;
+      keyY = 17;
+      doorX = 23;
+      doorY = 35;
+      buttonX = 43;
+      buttonY = 55;
+      button2X = 45;
+      button2Y = 55;
+      teleportX = 51;
+      teleportY = 3;
+      teleport2X = 30;
+      teleport2Y = 57;
+      doorOpen = false;
+      keyUsed = false;  
+      buttonPressed = false;
+      button2Pressed = false;
+      LevelMap()
+    } else if (event.key === "4") {
+      levelLogic = level4;
+      levelIndex = 4
+      playerX = 45;
+      playerY = 12;
+      keyX = null;
+      keyY = null;
+      doorX = null;
+      doorY = null;
+      buttonX = 43;
+      buttonY = 2;
+      button2X = 46;
+      button2Y = 2;
+      teleportX = null;
+      teleportY = null;
+      teleport2X = null;
+      teleport2Y = null;
+      doorOpen = false;
+      keyUsed = false;  
+      buttonPressed = false;
+      button2Pressed = false;
+      LevelMap()
+      Ethereo()
+    }
+    //level selector acima
+    
     if (
       !levelWall(newX, newY) &&
       !(doorX === newX && doorY === newY && !doorOpen)
     ) {
       playerX = newX;
       playerY = newY;
+    }
+    if (teleportX === newX && teleportY === newY && teleport2X !== null && teleport2X !== null) {
+      playerX = teleport2X
+      playerY = teleport2Y
+    }
+    if (teleport2X === newX && teleport2Y === newY && teleportX !== null && teleportX !== null) {
+      playerX = teleportX
+      playerY = teleportY
     }
 
     LevelMap();
